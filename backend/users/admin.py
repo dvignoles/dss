3f8ca5b1330718fbd5e6666b99217ce6035524cf
Neal Rea <nealrea818@gmail.com>
2018-11-21 15:16:18 -0500

    add accept/deny functionality for SU

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,10 +5,19 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+def promote_user(admin, request, queryset):
+	queryset.update(is_OU=True)
+promote_user.short_description = "Promote GU to OU"
+
+def demote_user(admin, request, queryset):
+	queryset.update(is_OU=False)
+demote_user.short_description = "Demote OU to GU"
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['username', 'interests', 'is_OU']
+    actions = [promote_user, demote_user]
 
 admin.site.register(CustomUser, CustomUserAdmin)
