be4442a84e7b5ac59c601fc267f1f5aa132df8e0
Neal Rea <nealrea818@gmail.com>
2018-11-25 03:06:14 -0500

    add document view - populates with document content

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('create/', views.CreateDoc, name='create'),
+    #path('view/', views.ViewDoc, name='view'),
+    url(r'^view/(?P<title>[\w\W]*)(?P<doc_id>[0-9]+)(?P<content>[\s\S]*)$', views.ViewDoc, name='view'),
 ]
