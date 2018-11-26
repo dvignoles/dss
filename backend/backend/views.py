535e8ccb015a78ca704e2f20e00eabac899b87c7
Neal Rea <nealrea818@gmail.com>
2018-11-25 21:12:05 -0500

    add interests to profile page

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -6,9 +6,15 @@ from django.views import generic
 from django.http import HttpResponse
 
 from documents.models import Document
+from documents.models import CustomUser
 
 def Profile(request):
 	#print(request.user.id)
+	user = CustomUser.objects.filter(id=request.user.id)
+	for u in user:
+		interests = u.interests
+	print(interests)
 	return render(request, 'profile.html', {
-    	'myDocs': Document.objects.filter(owner=request.user.id)
+    	'myDocs': Document.objects.filter(owner=request.user.id),
+    	'interests': interests,
     })
