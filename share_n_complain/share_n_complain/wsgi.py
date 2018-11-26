a83c43556e4505e9d2a971b5d56ae89713d65451
Neal Rea <nealrea818@gmail.com>
2018-11-26 00:28:11 -0500

    rename project

--- share_n_complain/share_n_complain/wsgi.py
+++ share_n_complain/share_n_complain/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'share_n_complain.settings')
 
 application = get_wsgi_application()
