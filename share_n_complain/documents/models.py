e35d3da3750bef887cdc6fb374d9a7621a0bb0f0
Neal Rea <nealrea818@gmail.com>
2018-11-29 22:04:09 -0500

    add History model to documents app

--- share_n_complain/documents/models.py
+++ share_n_complain/documents/models.py
@@ -8,7 +8,17 @@ class Document(models.Model):
 	title = models.CharField(max_length=100, default='')
 	content = models.CharField(max_length=10000, default='')
 	private = models.BooleanField('Classification', default=False)
+	version = models.IntegerField(default=1)
 	
 	def __str__(self):
 		doc = [self.owner, self.collaborators, self.content]
 		return doc
+
+class History(models.Model):
+	doc = models.ForeignKey(Document, null=False, on_delete=models.CASCADE)
+	vsn = models.IntegerField(null=False)
+	changes = models.CharField(max_length=100, null=False)
+	
+	def __str__(self):
+		history = [self.doc_id, self.vsn]
+		return history
