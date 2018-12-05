fb17224f2b4adf0194ae701607f4890ea65b02a6
Carlton Welch <welch.carlton@gmail.com>
2018-12-04 23:18:28 -0500

    Profile pictures added

--- share_n_complain/users/forms.py
+++ share_n_complain/users/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+import random as random
 
 class CustomUserCreationForm(UserCreationForm):
 
-    class Meta(UserCreationForm):
-        model = CustomUser
-        fields = ('username', 'interests')
+	class Meta(UserCreationForm):
+		model = CustomUser
+		fields = ('username', 'interests')
 
+		
 class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'interests')
+	
+	class Meta:
+		model = CustomUser
+		fields = ('username', 'interests')
--- share_n_complain/users/models.py
+++ share_n_complain/users/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import random as random
 
 class CustomUser(AbstractUser):
     # add additional fields in here
+    
     interests = models.CharField(max_length=100, default='')
     is_OU = models.BooleanField('OU status', default=False)
-
+    prof_pic_num = models.IntegerField(default=random.randrange(20))
+    
     def __str__(self):
         return self.username
